59d241e95dafb548311bd3b7a992fc369745fc8b
nexryai <nexryai@sda1.net>
2024-11-08 21:01:52 +0900

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,17 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    {
+        languageOptions: { globals: globals.browser }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: ["node_modules/*", "dist/*"],
+    },
+    {
+        files: ["**/*.ts"],
+    }
 ];
